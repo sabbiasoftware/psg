82201062d3ff2bed45e0494eb390b699d1ef74ed
Sabbia Soft <sabbiasoftware@gmail.com>
2026-02-20 21:42:48 +0100

    release

--- release/rel.py
+++ release/rel.py
@@ -8,7 +8,7 @@ nowstr = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
 reldir = f"psg_{nowstr}"
 
 os.system(f"{zipcmd} x -o{reldir} py.zip")
-shutil.copy(os.path.join("..", "sum.py"), reldir)
+shutil.copy(os.path.join("..", "psg.py"), reldir)
 shutil.copy(os.path.join("..", "psg.bat"), reldir)
 shutil.copytree(os.path.join("..", "cfg"), os.path.join(reldir, "cfg"), dirs_exist_ok=True)
 os.system(f"{zipcmd} a -tzip -r {reldir}.zip {reldir}")
