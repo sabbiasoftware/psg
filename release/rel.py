6d3db7689eaec878a3f71b88daf4ec7600e20b7d
Sabbia <sabbiasoftware@gmail.com>
2026-03-20 10:07:44 +0100

    Create empty users.txt in release

--- release/rel.py
+++ release/rel.py
@@ -32,7 +32,7 @@ os.mkdir(os.path.join(reldir, "cfg"))
 for fn in ["holidays.txt", "weekends.txt", "workingdays.txt"]:
     shutil.copy(os.path.join("..", "cfg", fn), os.path.join(reldir, "cfg"))
 
-for fn in ["projects.txt", "hotlines.txt", "userdata.csv"]:
+for fn in ["projects.txt", "users.txt", "hotlines.txt", "userdata.csv"]:
     Path(os.path.join(reldir, "cfg", fn)).touch()
 
 os.system(f"{zipcmd} a -tzip -r {reldir}.zip {reldir}")
